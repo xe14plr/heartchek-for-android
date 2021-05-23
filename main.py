cf845904e972d1042c33831d1de5b64218174c45
aarharov <algo.aarharov@gmail.com>
2021-05-23 13:58:46 +0300

    fix

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.uix.scrollview import ScrollView
-from kivy.core.image import Image as CoreImage
-
 from instructions import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
 from scrollLabel import ScrollLabel
 from ruffier import test
@@ -22,7 +20,6 @@ btn_color = (0, 0.9, 0.64, 1)
 age = 7
 name = ""
 p1, p2, p3 = 0, 0, 0
-im = CoreImage("paper.png")
 def check_int(str_num):
     try:
         return int(str_num)
@@ -282,11 +279,5 @@ class HeartCheck(App):
         sm.add_widget(Result(name='result'))
         return sm
 
-class Picture(Image):
-    pass
-
-class blablabla(HatWidget):
-    ...
-    self.add_widget(Picture(sourse='paper.png'))
 app = HeartCheck()
 app.run()
